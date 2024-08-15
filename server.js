bb5dd345236cdcd38c9b45be78cd316387fe646e
Martins98725 <icaro25martins@gmail.com>
2024-08-15 11:11:28 -0300

    colocando rotas no server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import http from 'http';
 
+const port = 3000
+
+const routes = {
+    '/': "Curso de node.js",
+    '/livros': 'teste de livros'
+};  
+
 const server = http.createServer((req, res) =>{
     res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('Curso node.js')
+    res.end(routes[req.url])
 });
-
-const port = 3000
 
 server.listen(port, () =>{
     console.log('Servidor rodando na porta 3000')
